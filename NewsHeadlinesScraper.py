12f47058b48b53dbc28a25c5ae552b52b2692610
Thomas Dolman <Thomasldolman@gmail.com>
2017-03-31 09:33:59 -0700

    Update NewsHeadlinesScraper.py

--- NewsHeadlinesScraper.py
+++ NewsHeadlinesScraper.py
@@ -2,45 +2,56 @@
 import bs4
 from urllib.request import urlopen as ureq
 from bs4 import BeautifulSoup as Soup
+import datetime
 
-#select an URL to download
-myurl ='https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38'
-myurl
+# in the future create a for loop to go to all the websites of interst
+# current sites of interest
+strWebsites = ["http://nos.nl/", "http://www.volkskrant.nl/"]
+strNamesFiles = ["NOS", "VK"]
+
+for loopNum in range(0,len(strWebsites)):
+        #open a file to store the data in
+        filename = "Headlines_" + str(strNamesFiles[loopNum]) + ".csv"
+        f = open(filename, "a") #tw tells the mode
+        
+        #write in the filename
+        headers = "Title, Intro," + str(datetime.datetime.now()) + "\n"
+        f.write(headers)
+
+        #select an URL to download
+        myurl = strWebsites[loopNum]
+        
+        #oppening a connection, grabbing the page
+        uClient = ureq(myurl)
+        page_html = uClient.read()
+        uClient.close()
+
+        #html parsing
+        page_soup = Soup(page_html, "html.parser")
+
+        #grab First header on the page
+        #First_header = page_soup.h1.text.strip()
+        #f.write(First_header.replace(",",">"))
+
+        #grabs each header list
+        if myurl == strWebsites[0]:
+                containers = page_soup.findAll("div",{"class":"list-featured__wrap"})
+        elif myurl == strWebsites[1]:
+                containers = page_soup.findAll("header",{"class":"ankeiler-teaser__header"})
+
+        #grab the relevant info
+        for contain in containers:
+                if myurl == strWebsites[0]:
+                        Article_Title = contain.span.text
+                        Article_Intro = contain.p.text
+                elif myurl == strWebsites[1]:
+                        Article_Title = contain.h2.text
+                        Article_Intro = " "
+                        
+##	print("Title: " + Article_Title)
+##	print("Article Intro:" + Article_Intro)
 	
+                f.write(Article_Title.replace(",",">") + "," + Article_Intro.replace(",",">") + "\n")
 
-#oppening a connection, grabbing the page
-uClient = ureq(myurl)
-page_html = uClient.read()
-uClient.close()
+        f.close()
 
-#html parsing
-page_soup = Soup(page_html, "html.parser")
-
-#grabs each product
-container = page_soup.findAll("div",{"class":"item-container"})
-
-#open a file to store the data in
-filename = "products.csv"
-f = open(filename, "w") #tw tells the mode
-
-#write in the file
-headers = "brand, product_name,shipping_cost\n"
-f.write(headers)
-
-#grab the brand
-for contain in container:
-	brand  = contain.div.div.a.img["title"]
-
-	title_containe = contain.findAll("a",{"class":"item-title"})
-	prod_name = title_containe[0].text
-
-	shipping_container = contain.findAll("li",{"class":"price-ship"})	
-	shipp = shipping_container[0].text.strip()
-
-	print("brand: " + brand)
-	print("product name:" + prod_name)
-	print("shipping cost: " + shipp)
-
-	f.write(brand + "," + prod_name.replace(",","|") + ","+ shipp + "\n")
-
-f.close()
